50b62fed803b3b8bffa85d0d719f33a694a00069
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-08-24 18:47:56 -0500

    added photo

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,24 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+$(document).ready(function () {
+
+  $('#search').on('submit', function() {
+    var stars = $('#search-name').val();
+
+    $.ajax({
+      url:'http://www.strudel.org.uk/lookUP/json/?name='+stars,
+      dataType: "jsonp",
+      jsonpCallback: "logResults"
+    })
+
+    .fail(function() {
+      alert('error')
+    })
+    return false;
+
+  });
+});
+
+function logResults(json) {
+  $('#preview').html('<img src="' + json.image.src + '">');
+  console.log(json);
+
 }
